d9dbeae2a42c263a2ff2437fcb9233437a356688
tanczikm <tanczikm@kkszki.hu>
2025-09-11 09:39:39 +0200

    2025. 09. 11.

---
+++ adatok.py
@@ -0,0 +1,15 @@
+# int - egész szám
+szam = 5
+print(type(szam))
+
+# float - tizedes tört
+tizedes_tort = 0.25
+print(type(tizedes_tort))
+
+# str - karaktersorozat
+szoveg = "alma"
+print(type(szoveg))
+
+# bool - két értéket vehet fel: True vagy False
+oszthato = True
+print(type(oszthato))
---
+++ elso2.py
@@ -0,0 +1,6 @@
+nev = input("Hogy hívnak?") #itt adatot kérünk be a felhasználótól
+print(nev) #kiírom a változó értékét
+print("Jó reggelt,", nev, "!")
+print("Jó reggelt, ", '', nev, "!", sep='')
+print("Jó reggelt, ", '', nev, "!", sep='***')
+print(f"Jó reggelt, {nev}!")
