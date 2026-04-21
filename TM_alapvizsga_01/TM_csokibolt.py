64d6f0236c3ebfb01b5100729e67c055ca09d71c
tanczikm <tanczikm@kkszki.hu>
2026-04-21 14:11:37 +0200

    2026. 04. 21.

--- TM_alapvizsga_01/TM_csokibolt.py
+++ TM_alapvizsga_01/TM_csokibolt.py
@@ -1,30 +1,47 @@
 from gyakorlo_3_18pont_v2 import csoki
 
 celfajl = open("csokik.txt", "r", encoding="utf-8")
-fehercsoki_tomegek = []
-papircsomagolasu = 0
+csokik = []
 
 for sor in celfajl:
-    sor = sor.strip()
-    sor = sor.split(";")
-    
-    if sor[0] == "fehér":
-        fehercsoki_tomegek.append(int(sor[1]))
-    if sor[2] == "papír":
-        papircsomagolasu = papircsomagolasu + 1
+    fajta, tomeg, csomag = sor.strip().split(";")
+    uj_csoki = csoki(fajta, int(tomeg), csomag)
+    csokik.append(uj_csoki)    
 
 celfajl.close()
 
+feher_csokik = []
+for cs in csokik:
+    if cs.tipus == "fehér":
+        feher_csokik.append(cs.tomeg)
 
-fehercsoki_atlag = sum(fehercsoki_tomegek) / len(fehercsoki_tomegek)
-print(f"A fehér csokik átlagos tömege: {round(fehercsoki_atlag, 1)}g")
+if len(feher_csokik) > 0:
+    fehercsoki_atlag = sum(feher_csokik) / len(feher_csokik)
+    print(f"A fehér csokik átlagos tömege: {round(fehercsoki_atlag, 1)}g")
+else:
+    print("Nincs fehér csoki az adatok között.")
 
-print(f"Összesen {papircsomagolasu} papír csomagolású csoki van.")
+papir_db = 0
+for cs in csokik:
+    if cs.csomagolas == "papír":
+        papir_db = papir_db + 1
+
+print(f"Összesen {papir_db} papír csomagolású csoki van.")
 
 
-csoki_tipus = input("Kérem, adja meg a csoki típusát: ")
-csoki_tomeg = input("Kérem, adja meg a csoki tömegét: ")
-csoki_csomagolas = input("Kérem, adja meg a csoki csomagolását: ")
-csoki_adag = int(input("Kérem, adja meg hány adagot szeretne: "))
+tipus = input("Kérem, adja meg a csoki típusát: ")
+tomeg = int(input("Kérem, adja meg a csoki tömegét: "))
+csomag = input("Kérem, adja meg a csoki csomagolását: ")
+mennyiseg = int(input("Kérem, adja meg hány adagot szeretne: "))
 
-print(csoki.ar(csoki(csoki_tipus, csoki_tomeg, csoki_csomagolas)))
+talalat = None
+for cs in csokik:
+    if cs.tipus == tipus and cs.tomeg and cs.csomagolas == csomag:
+        talalat = cs
+        break
+
+if talalat is not None:
+    osszar = talalat.ar() * mennyiseg
+    print(f"A termék ({tipus}, {tomeg}g, {csomag} csomagolás, {mennyiseg} adag) ára: {osszar} Ft")
+else:
+    print("Hiánycikk.")
